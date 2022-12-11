fbfba09c523d14a077377d1cfebe44615a336dc2
Tokunbo <toxylee20021@gmail.com>
2022-12-11 00:20:31 -0800

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like buidling a multilingua puzzle");
+	return (0);
+
+}
