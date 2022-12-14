8f4f73271d1537d8fff8b5a69b1eebb39fd148b6
Tokunbo <toxylee20021@gmail.com>
2022-12-14 06:33:39 -0800

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,13 +5,13 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (success)
  *
  */
 
 int main(void)
-{
 
+{
 	int n;
 
 	srand(time(0));
@@ -19,9 +19,9 @@ int main(void)
 
 	if (n > 0)
 	{
-		printf("%d is posive\n", n);
+		printf("%d is positive\n", n);
 	}
-	else if (n == 0)
+	else if (n ==0)
 	{
 		printf("%d is zero\n", n);
 	}
