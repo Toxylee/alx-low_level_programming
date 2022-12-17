6bc7edcdd7509b3d37a0e76f3799f23593fdb85d
Tokunbo <toxylee20021@gmail.com>
2022-12-17 04:02:35 -0800

    task8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,12 +4,11 @@
 * jack_bauer - prints every minute of the day
 * Return: Always 0
 */
-
 void jack_bauer(void)
 {
 	int a;
 	int b;
-	
+
 	for (a = 0; a <= 23; a++)
 	{
 		for (b = 0; b <= 59; b++)
@@ -23,6 +22,3 @@ void jack_bauer(void)
 		}
 	}
 }
-
-
-
