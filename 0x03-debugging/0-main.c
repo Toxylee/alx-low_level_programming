814463be9bd77435128c62387ce315fbd147cb1a
Tokunbo <toxylee20021@gmail.com>
2022-12-18 05:08:29 -0800

    task00

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,7 +2,7 @@
 
 /**
 * main - tests function that prints if integer is positive or negative
-*Return: 0
+*Return: Always 0 (Success)
 */
 
 int main(void)
@@ -11,6 +11,5 @@ int main(void)
 	
 	i = 0;
 	positive_or_negative(i);
-
 	return (0);
 }
