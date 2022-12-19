7a9f1f44271903eb0ec107922594bef206e4eccd
Tokunbo <toxylee20021@gmail.com>
2022-12-19 14:08:07 -0800

    Task6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,9 +9,7 @@ i#include "main.h"
  * @str: input
  *
  * Return: print
- *
  */
-
 void puts2(char *str)
 {
 	int longi = 0;
@@ -24,9 +22,8 @@ void puts2(char *str)
 		y++;
 		longi++;
 	}
-	
-	t = longi - 1;
-	for (o = 0 ; o <= t ; o++)
+		t = longi - 1;
+		for (o = 0 ; o <= t ; o++)
 	{
 		if (o % 2 == 0)
 		{
