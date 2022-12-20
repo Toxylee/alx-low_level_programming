b958dc00b628c8831975dd9551138c9fae4592f0
Tokunbo <toxylee20021@gmail.com>
2022-12-20 06:12:09 -0800

    tAsk6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,8 +5,6 @@
 */
 void print_line(int n)
 {
-	int n;
-
 	if (n <= 0)
 	{
 		_putchar('\n');
@@ -19,6 +17,6 @@ void print_line(int n)
 		{
 			_putchar('_');
 		}
-		_putchar('\n');
+	_putchar('\n');
 	}
 }
