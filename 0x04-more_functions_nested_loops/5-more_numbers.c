8c4c2c3ae21b0dcbc782dd88f092cfdad6e50590
Tokunbo <toxylee20021@gmail.com>
2022-12-20 06:23:15 -0800

    task5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,8 +12,8 @@ void more_numbers(void)
 	for (y = 0; y <= 15; y++)
 	{
 	if (y > 10)
-		_putchar(y / 10 + '0');
-		_putchar(y % 10 + '0');
+	_putchar(y / 10 + '0');
+	_putchar(y % 10 + '0');
 	}
 	_putchar('\n');
 	}
