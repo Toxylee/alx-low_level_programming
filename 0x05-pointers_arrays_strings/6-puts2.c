43a543e92fd196f96f2b1cdf99cb565b6167d15b
Tokunbo <toxylee20021@gmail.com>
2022-12-20 03:44:42 -0800

    task6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,22 +1,17 @@
-i#include "main.h"
-
+#include "main.h"
 /**
- *
- * puts2 - function should print only one character out of two
- *
- * starting with the first one
- *
- * @str: input
- *
- * Return: print
- */
+*puts2 - function should print only one character out of two
+*starting with the first one
+*@str: input
+*Return: print
+*/
 void puts2(char *str)
 {
 	int longi = 0;
 	int t = 0;
 	char *y = str;
 	int o;
-	
+
 	while (*y != '\0')
 	{
 		y++;
