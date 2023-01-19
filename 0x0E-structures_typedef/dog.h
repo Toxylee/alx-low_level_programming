efdb7377f070fd0c202ceb42e1caa18db4da7b00
Tokunbo <toxylee20021@gmail.com>
2023-01-19 11:58:30 -0800

    Task0-5

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,7 +7,6 @@
  *@owner: Third member
  *Description: Longer description
  */
-
 struct dog
 {
 	char *name;
@@ -15,8 +14,8 @@ struct dog
 	char *owner;
 };
 /**
- * dog_t - typedef for struct dog
- * */
+ *dog_t - typedef for struct dog
+ */
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
