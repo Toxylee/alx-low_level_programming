1c98ec1ca5f8ea5d54e754867fe0bb0553f91a4c
Tokunbo <toxylee20021@gmail.com>
2023-01-26 16:24:15 -0800

    Task 0-101

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ *print_list - print all the elements of a structure list
+ *@h: the structure pointer
+ *Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t counter = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+			h = h->next;
+		}
+		counter++;
+	}
+	return (counter);
+}
